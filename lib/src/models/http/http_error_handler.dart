eee635f8dcae9db4b64d4ba0ac2afdf9c2ed74ee
shajed99-Polygon <shajedurrahmanpanna.panna@gmail.com>
2025-03-20 19:44:53 +0600

    Update http_error_handler.dart

--- lib/src/models/http/http_error_handler.dart
+++ lib/src/models/http/http_error_handler.dart
@@ -1,15 +1,40 @@
 import 'package:dio/dio.dart';
 import 'package:getx_mvc/src/models/http/http_error_enum.dart';
+import 'package:getx_mvc/src/utils/dev_functions/dev_print.dart';
+import 'package:getx_mvc/src/utils/user_message/snackbar.dart';
 
-// class HttpErrorHandler {
-//   Future<HTTPErrorEnum> errorHandler({
-//     bool showError = true,
-//     required Future<void> Function() function,
-//   }) async {
-//     try {
-//       await function();
-//       return HTTPErrorEnum.done;
-//     } on DioException catch (e) {
-//     } catch (e) {}
-//   }
-// }
+class HttpErrorHandler {
+  Future<HTTPErrorEnum> errorHandler({
+    bool showError = true,
+    required Future<void> Function() function,
+  }) async {
+    HTTPErrorEnum? error;
+    try {
+      await function();
+      return HTTPErrorEnum.done;
+    } on DioException catch (e) {
+      error = _getErrorType(e.response?.statusCode);
+    } catch (e) {
+      if (e is Response) {
+        error = _getErrorType(e.statusCode);
+      }
+    }
+
+    final HTTPErrorEnum resultError = error ?? HTTPErrorEnum.unknown;
+
+    if (showError) {
+      showSnackBar(title: 'Error', message: resultError.errorMessage);
+    }
+
+    return resultError;
+  }
+}
+
+HTTPErrorEnum? _getErrorType(int? statusCode) {
+  if (statusCode == null) return null;
+  for (final HTTPErrorEnum error in HTTPErrorEnum.values) {
+    if (error.errorCode == statusCode) {
+      return error;
+    }
+  }
+}
